22a30a5d0c39d87a0197362cc28716d406c0f777
juti <69962698+wamCEO@users.noreply.github.com>
2020-08-20 12:11:09 +0300

    Add files via upload

---
+++ unbanz.bat
@@ -0,0 +1,116 @@
+@echo off
+color 4 
+cls 
+:: auto elevates to admin for ease of use 
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+ goto :start
+
+:start 
+
+cls
+echo.
+color a
+echo zxc213d
+color 4
+echo.
+echo.
+echo 1 - clean folders
+echo 2 - clean xbox stuff
+echo.
+choice /c:123 /n
+if %errorlevel% equ 1 goto :folderclean
+if %errorlevel% equ 2 goto :xboxclean
+
+:folderclean
+call :getDiscordVersion
+cls
+del /s /f /q "%appdata%\CitizenFX" >nul 2>nul 
+del /s /f /q "%appdata%\DigitalEntitlements" >nul 2>nul
+del /s /f /q "%appdata%\Discord\%discordVersion%\modules\discord_rpc" >nul 2>nul
+call :errorcheck
+cls
+echo. 
+echo done
+pause >nul
+goto :start  
+:getDiscordVersion
+for /d %%a in ("%appdata%\Discord\*") do (
+   set "varLoc=%%a"
+   goto :d1
+)
+:d1
+for /f "delims=\ tokens=7" %%z in ('echo %varLoc%') do (
+   set "discordVersion=%%z"
+)
+goto :EOF
+
+:xboxclean
+cls
+powershell -Command "& {Get-AppxPackage -AllUsers xbox | Remove-AppxPackage}" >NUL 2>NUL
+sc stop XblAuthManager >NUL 2>NUL
+sc stop XblGameSave >NUL 2>NUL
+sc stop XboxNetApiSvc >NUL 2>NUL
+sc stop XboxGipSvc >NUL 2>NUL
+sc delete XblAuthManager >NUL 2>NUL
+sc delete XblGameSave >NUL 2>NUL
+sc delete XboxNetApiSvc >NUL 2>NUL
+sc delete XboxGipSvc >NUL 2>NUL
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\xbgm" /f >NUL 2>NUL
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTask" /disable >NUL 2>NUL
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTaskLogon" /disable >NUL 2>NUL
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f >NUL 2>NUL
+call :errorcheck
+cls
+echo. 
+echo xbox cleaning finished
+pause >nul
+goto :start  
+
+:errorcheck 
+if %errorlevel% EQU 1 (
+    cls
+    echo Failed. 
+    timeout /t 3 >nul 2>nul
+    exit /b
+) else (
+    rem asdfgh
+    goto :eof
+)
